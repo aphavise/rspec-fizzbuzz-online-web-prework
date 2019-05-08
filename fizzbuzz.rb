01f8bc9646e5f241a94e6725aa3166bf8c1a9e6f
reliable-caller-8794 <andrewphaviseth@gmail.com>
2019-05-08 23:52:04 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-require 'pry'
-
 def fizzbuzz(int)
   if int % 3 == 0
    return "Fizz"
   elsif int % 5 == 0 
    return "Buzz"
-binding.pry
   elsif int % 3 == 5 && int % 5 == 3
    return "FizzBuzz"
   elsif int % 4 == 0 
